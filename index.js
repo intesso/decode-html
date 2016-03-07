3f03b8bb5f46afbf5dc741e2b97ce7db95ebc16a
maciejhirsz <maciej.hirsz@gmail.com>
2016-03-07 11:41:55 +0100

    reworked for performance and more entities

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+// Store markers outside of the function scope,
+// not to recreate them on every call
+var entities = {
+  'amp': '&',
+  'apos': '\'',
+  'lt': '<',
+  'gt': '>',
+  'quot': '"',
+  'nbsp': '\xa0'
+};
+var entityPattern = /&([a-z]+);/ig;
+
 module.exports = function decodeHTMLEntities(text) {
-  var replacements = [
-    ['amp', '&'],
-    ['apos', '\''],
-    ['lt', '<'],
-    ['gt', '>']
-  ];
-
-  replacements.forEach(function(replace){
-    text = text.replace(new RegExp('&'+replace[0]+';', 'g'), replace[1]);
+  // A single replace pass with a static RegExp is faster than a loop
+  return text.replace(entityPattern, function(match, entity) {
+    // `null` will only match `null` or `undefined` on type coercion
+    if (entities[entity] != null) {
+      return entities[entity];
+    }
+    // return original string if there is no matching entity (no replace)
+    return match;
   });
-
-  return text;
 };
