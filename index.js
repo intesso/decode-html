98dfb6ae05873b40f0002f8228bef9df17696567
maciejhirsz <maciej.hirsz@gmail.com>
2017-08-28 16:09:11 +0200

    Re-use .toLowerCase()

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ var entityPattern = /&([a-z]+);/ig;
 module.exports = function decodeHTMLEntities(text) {
   // A single replace pass with a static RegExp is faster than a loop
   return text.replace(entityPattern, function(match, entity) {
-    if (entities.hasOwnProperty(entity.toLowerCase())) {
+    entity = entity.toLowerCase();
+    if (entities.hasOwnProperty(entity)) {
       return entities[entity];
     }
     // return original string if there is no matching entity (no replace)
